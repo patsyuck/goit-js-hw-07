31c101ced5ae845d95d46e654ff6681acb4f5753
Oleksii Patsiuk <patsyuck@ukr.net>
2021-02-21 19:41:22 +0200

    Add task 4

---
+++ js/task-4.js
@@ -0,0 +1,14 @@
+const counter = document.querySelector('div#counter').querySelector('span#value')
+let counterValue = Number(counter.textContent)
+const increment = () => {
+    counterValue = counterValue + 1
+    counter.textContent = counterValue
+}
+const decrement = () => {
+    counterValue = counterValue - 1
+    counter.textContent = counterValue
+}
+const incrementNode = document.querySelector('div#counter').querySelector('button[data-action="increment"]')
+const decrementNode = document.querySelector('div#counter').querySelector('button[data-action="decrement"]')
+incrementNode.addEventListener('click', function () { increment() })
+decrementNode.addEventListener('click', function () { decrement() })
