fa01cba21d17027d1f7e1473b2848507a056e742
Oleksii Patsiuk <patsyuck@ukr.net>
2021-02-21 13:48:08 +0200

    Add task 2

---
+++ js/task-2.js
@@ -0,0 +1,10 @@
+const ingredients = ['Картопля', 'Гриби', 'Часник', 'Помідори', 'Зелень', 'Приправи']
+
+const nodes = ingredients.map(elem => {
+    const node = document.createElement('li')
+    node.textContent = elem
+    return node
+})
+
+const ingredientsNodes = document.querySelector('ul#ingredients')
+nodes.forEach(node => ingredientsNodes.appendChild(node))
